b84319c6e9d4f3383a2477404d637128b31d6cec
hikaruk <h.kurosawa@mail.utoronto.ca>
2025-07-02 11:10:46 -0400

    update

--- DataImport.py
+++ DataImport.py
@@ -516,7 +516,7 @@ class Operations():
             self.history = self.modelD.fit([self.OP, self.FL], [self.QF, self.DF],validation_split=0.2,batch_size=self.params['batch'],
                                     epochs=50, verbose=1, shuffle=True, callbacks=callbackList)     
         else:
-
+            print("mask shape: ", self.mask.shape)
             self.history = self.modelD.fit([self.OP, self.FL], [self.QF, self.DF, self.mask],validation_split=0.2,batch_size=self.params['batch'],
                                     epochs=self.params['epochs'], verbose=1, shuffle=True, callbacks=callbackList)    
         
