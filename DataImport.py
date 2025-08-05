546d39afbc5306def975de2887e28219bfdfac84
hikaruk <h.kurosawa@mail.utoronto.ca>
2025-08-05 11:21:39 -0400

    update

--- DataImport.py
+++ DataImport.py
@@ -134,7 +134,7 @@ class Operations():
 
 
 
-    def importData(self, isTesting):
+    def importData(self,isTesting=True,quickTest=False):
         # Ask user where to load data from
         #source = input("Load data from [s3/local]: ").strip().lower()
 
